55ae83b6686a910fad1fce8da338840aae92dab3
Rizal Mantopani <mntpnijal@gmail.com>
2022-10-21 15:04:16 +0700

    mini chalenge

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Utama from "./components/utama";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-     <Utama/>
-    </div>
+  <div>
+    <Router>
+      <Routes>
+        <Route path="/" exact component={Utama} />
+      </Routes>
+    </Router>
+  </div>
   );
 }
 
--- src/components/utama.js
+++ src/components/utama.js
@@ -1,25 +1,28 @@
 import React from 'react'
 import {dummy} from './dummy'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useNavigate } from 'react-router-dom';
 
 const utama = () => {
+    const navigate = useNavigate()
+    return dummy?.map((item) => {
   return (
       <div>
       <div>
         <h1>Instagram</h1>
-        {dummy.map((item, index) => ( 
-        <div className='w-full d-flex'>
+        
+        <div key={item.id} className='w-full d-flex' onClick={() => navigate(`/profile/${item.id}`)}>
         <img src={item.image} className=" w-25 rounded-5 mb-3" alt=""/>
         <h1>{item.username}</h1>
         <h2>{item.follower}</h2>
       
         </div>
            
-        ))}
         
     </div>
     </div>
-  )
+  )}
+    )
 }
 
 export default utama
